a9b5c088a5c01ed114657ac00d7fa01d115bcb8a
Krasu <krasniyrus@gmail.com>
2013-10-19 11:06:55 +0400

    minor tweaks

--- js/switches.js
+++ js/switches.js
@@ -1,25 +1,27 @@
-(function($) {
-	var iOSFlipSwitch = function(el, options) {
+(function ($) {
+	var iOSFlipSwitch = function (el, options) {
 		this.input = el
-		this.state = {}
+		this.dragState = {}
 		this.options = $.extend({
 			containerClass: 'ios-switch-container',
 			handleClass: 'switcher',
-			duration: 200
+			onClass: 'on',
+			offClass: 'off',
+			animationDuration: 200
 		}, options);
 
 		this.init()
 
-		return {
-			reset: $.proxy(this.calcMetrics, this)
+		this.input.data().iOSFlipSwitch = {
+			reset: $.proxy(this.handleChange, this)
 		}
 	}
 
-	iOSFlipSwitch.prototype.init = function() {
-		this.container = $('<div>').addClass(this.options.containerClass);
-		this.on = $('<div class="on">');
-		this.off = $('<div class="off">');
-		this.handle = $('<div>').addClass(this.options.handleClass);
+	iOSFlipSwitch.prototype.init = function () {
+		this.container = $('<div>').addClass(this.options.containerClass)
+		this.on = $('<div>').addClass(this.options.onClass)
+		this.off = $('<div>').addClass(this.options.offClass)
+		this.handle = $('<div>').addClass(this.options.handleClass)
 
 		this.container.append([this.on, this.handle, this.off])
 
@@ -28,12 +30,12 @@
 
 		this.calcMetrics()
 
-		this.container.on('mousedown touchstart', $.proxy(this.onDragStart, this));
-		this.input.on('change', $.proxy(this.handleChange, this));
+		this.container.on('mousedown touchstart', $.proxy(this.onDragStart, this))
+		this.input.on('change', $.proxy(this.handleChange, this))
 		this.setPositions(false, false)
 	}
 
-	iOSFlipSwitch.prototype.calcMetrics = function() {
+	iOSFlipSwitch.prototype.calcMetrics = function () {
 		if (this.metrics && this.metrics.containerWidth) return
 
 		this.metrics = {
@@ -44,82 +46,93 @@
 		this.metrics.maxX = this.metrics.containerWidth - this.metrics.handleWidth;
 	}
 
-	iOSFlipSwitch.prototype.handleChange = function() {
+	iOSFlipSwitch.prototype.handleChange = function () {
+		this.calcMetrics()
 		this.setPositions(false, true)
 	}
 
-	iOSFlipSwitch.prototype.setPositions = function(x, animate) {
+	iOSFlipSwitch.prototype.setPositions = function (x, animate) {
 		if (x === false) {
 			x = this.input.prop('checked') ? this.metrics.maxX : 0
 		}
+
 		if (x < -1) x = -1
 		if (x > this.metrics.maxX) x = this.metrics.maxX
 
 		var onX = (x - this.metrics.containerWidth) + this.metrics.handleWidth,
-			duration = animate ? this.options.duration : 0;
+			duration = animate ? this.options.animationDuration : 0;
 
 		this.handle.animate({
 			left: x
-		}, duration);
+		}, duration)
 		this.off.animate({
 			left: x
-		}, duration);
+		}, duration)
 		this.on.animate({
 			left: onX
 		}, duration)
 	}
 
-	iOSFlipSwitch.prototype.getActualX = function(event) {
+	iOSFlipSwitch.prototype.getActualX = function (event) {
 		if (event.pageX) return event.pageX
-		if (event.originalEvent.changedTouches) return event.originalEvent.changedTouches[0].pageX
+		if (event.originalEvent.changedTouches) {
+			return event.originalEvent.changedTouches[0].pageX
+		}
 		return 0
 	}
 
-	iOSFlipSwitch.prototype.onDragEnd = function(event) {
-		$(document).off('mousemove touchmove', $.proxy(this.onDrag, this));
-		$(document).off('mouseup touchend', $.proxy(this.onDragEnd, this));
+	iOSFlipSwitch.prototype.onDragEnd = function (event) {
+		$(document).off('mousemove touchmove', $.proxy(this.onDrag, this))
+		$(document).off('mouseup touchend', $.proxy(this.onDragEnd, this))
 
-		if (!this.state.currentlyClicking) return;
-		event.preventDefault();
+		if (!this.dragState.currentlyClicking) return;
+
+		event.preventDefault()
 
 		var checked
-		if (this.state.dragging) {
-			var p = (this.getActualX(event) - this.state.dragStartPosition) / this.metrics.maxX
-			checked = p >= 0.5;
+		if (this.dragState.dragging) {
+			var position = (this.getActualX(event) - this.dragState.dragStartPosition)
+			checked = position / this.metrics.maxX >= 0.5;
 		} else {
-			checked = !this.input.prop('checked');
+			checked = !this.input.prop('checked')
 		}
 
-		this.input.prop('checked', checked);
-		this.state.currentlyClicking = null;
-		this.state.dragging = null;
-		this.setPositions(false, true)
+		this.input.prop('checked', checked)
+		this.dragState = {}
+		this.input.trigger('change')
 	}
 
-	iOSFlipSwitch.prototype.onDragStart = function(event) {
-		event.preventDefault();
-		$(document).on('mousemove touchmove', $.proxy(this.onDrag, this));
-		$(document).on('mouseup touchend', $.proxy(this.onDragEnd, this));
+	iOSFlipSwitch.prototype.onDragStart = function (event) {
+		event.preventDefault()
+		$(document).on('mousemove touchmove', $.proxy(this.onDrag, this))
+		$(document).on('mouseup touchend', $.proxy(this.onDragEnd, this))
 
 
-		this.state.currentlyClicking = this.handle;
-		this.state.dragStartPosition = this.getActualX(event);
-		this.state.handleLeftOffset = parseInt(this.handle.css('left'), 10) || 0;
+		this.dragState.currentlyClicking = this.handle
+		this.dragState.dragStartPosition = this.getActualX(event)
+		this.dragState.handleLeftOffset = parseInt(this.handle.css('left'), 10) || 0
 	}
 
-	iOSFlipSwitch.prototype.onDrag = function(event) {
-		event.preventDefault();
-		if (!this.state.currentlyClicking) return;
+	iOSFlipSwitch.prototype.onDrag = function (event) {
+		event.preventDefault()
+		if (!this.dragState.currentlyClicking) return;
+
 		var x = this.getActualX(event)
 
-		this.state.dragging = (!this.state.dragging && Math.abs(this.state.dragStartPosition - x))
+		this.dragState.dragging = (!this.dragState.dragging && Math.abs(this.dragState.dragStartPosition - x))
 
-		this.setPositions(x + this.state.handleLeftOffset - this.state.dragStartPosition);
+		this.setPositions(x + this.dragState.handleLeftOffset - this.dragState.dragStartPosition)
 	}
 
-	$.fn.iOSFlipSwitch = function(options) {
-		return $(this).each(function(i, el) {
-			new iOSFlipSwitch($(el), options)
+	$.fn.iOSFlipSwitch = function (options) {
+		return $(this).each(function (i, el) {
+			el = $(el)
+			var instance = el.data().iOSFlipSwitch
+			if (!instance) {
+				new iOSFlipSwitch($(el), options)
+			} else if (instance[arguments[0]]) {
+				instance[arguments[0]].apply(Array.prototype.splice.call(arguments, 1))
+			}
 		})
 	}
 
